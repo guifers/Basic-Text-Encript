9ebaaf965151c3f3f9590800494d048b3c998635
Guille <guifers23@gmail.com>
2022-03-08 19:06:59 +0000

    Ficheros base de 2017

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(3, 69)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(369, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(136, 120)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(93, 50)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "DO"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(3, 205)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(369, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(489, 120)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(104, 50)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "UNDO"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(378, 205)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(336, 20)
+        Me.TextBox3.TabIndex = 7
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(378, 69)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(336, 20)
+        Me.TextBox4.TabIndex = 8
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(335, 125)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(73, 45)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "HELP"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(717, 245)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Easy Encrypt"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button4 As Button
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,182 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Para encriptar se coge la equivalencia del carácter introducido en ASCII y se le suma un número aleatorio
+        'generado(volare) y con dicho resultado se genera el nuevo caracter
+        Dim Longitud As Integer
+        Dim A As String
+        A = TextBox1.Text
+        Longitud = A.Length
+        Dim array(Longitud) As String
+        Dim array2(Longitud) As String
+        Dim array3(Longitud) As String
+        Dim array4(Longitud) As String
+        Dim array5(Longitud) As String
+        Dim numeroAleatorio As New Random()
+        Dim volare As Integer
+        Dim ruta As String = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\4decript.txt" 'Probar a saltar restriccion para crear file en C;/
+        Dim escritor As New System.IO.StreamWriter(ruta)
+
+        For i = 1 To Longitud Step 1
+            array(i) = Mid(A, i, 1)
+            TextBox2.Text = array(i)
+
+        Next
+
+        For Z = 1 To 1 Step 1
+
+
+            For I = 1 To Longitud Step 1
+                volare = numeroAleatorio.Next(1, 10)
+                escritor.WriteLine(volare)
+                array2(Z) = AscW(array(I))
+                'MsgBox(array2(Z)) #Ver porque no worka esto -EN Z ES UN SOLO LOOP
+                array3(I) = array2(Z) + volare
+                array4(I) = ChrW(array3(I))
+                'MsgBox(volare)
+                'MsgBox(array2(Z))
+                'MsgBox(array3(I))
+                'MsgBox(array4(I))
+
+
+            Next
+        Next
+        escritor.Close()
+        array5(Longitud) = String.Join("", array4)
+        TextBox2.Text = array5(Longitud)
+
+        'MsgBox(array4.Length)
+        'MsgBox(array5.Length)
+
+
+        'Dim Hello As String
+        'Dim C As Integer
+        'Hello = array(1)
+        'MsgBox(Hello)
+        'Dim B As Object
+        'B = AscW(Hello)
+        'MsgBox(array.Length - 1)
+        'MsgBox(A.Length)
+        'MsgBox(Chr(array(1)))
+        'For z = 1 To Longitud Step 1
+        'array2(z) = Chr(array(z))
+        'MsgBox(array2(z))
+        'Next
+        'Dim FirstWord As String = Mid(A, 1, 1)
+        'Label2.Text = array
+
+
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'Para desencriptar se precisa del fichero 4decript que contiene los numeros aleatorios generados en el proceso
+        'de encriptado.
+        Dim rutita As String = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\4decript.txt" 'Probar a saltar restriccion para crear file en C;/
+        Dim lector As New StreamReader(rutita)
+        Dim lector2 As New StreamReader(rutita) 'El cabron no me dejaba volver a leerlo con el mismo lector
+
+        Dim Longitud As Integer
+        Dim B = TextBox4.Text
+        Longitud = B.Length
+
+
+        Dim contador = 0
+        'Dim linea = lector.ReadLine ' Cuando el método ReadLine alcanza el final del archivo, devuelve Nothing.
+        If File.Exists(rutita) Then
+            MsgBox("FILE FOUND!")
+            While lector.Peek <> -1 'devuelve -1 es que no leyo nah
+
+                contador = contador + 1
+                Dim linea = lector.ReadLine ' Cuando el método ReadLine alcanza el final del archivo, devuelve Nothing.
+                'MsgBox(linea)
+
+            End While
+            lector.Close()
+            Dim arrayconverse(contador) As String
+            Dim arrayconverse2(contador) As String
+            Dim array(contador) As String
+            Dim array2(contador) As String
+            Dim arrayspec(contador) As String
+            Dim arrayline(contador) As String
+            Dim arrayfinal(contador) As String
+            Dim array57(contador) As String
+            For i = 1 To contador Step 1
+                array(i) = Mid(B, i, 1)
+                'MsgBox(array(i))
+            Next
+            'While lector.EndOfStream ¿Como tomar el control del programa para meterle datos?
+            'While lector2.Peek <> -1 'devuelve -1 es que no leyo nah
+            'Dim linea = lector2.ReadLine ' Cuando el método ReadLine alcanza el final del archivo, devuelve Nothing.
+            'MsgBox(linea)
+            For R = 1 To contador Step 1
+                If lector2.Peek <> -1 Then
+                    Dim linea = lector2.ReadLine
+                    arrayline(R) = linea
+                    'MsgBox(arrayline(R))
+                Else
+                    Exit For
+                End If
+            Next
+            Dim suma As Integer
+            For v = 1 To contador Step 1
+                For X = 1 To contador Step 1
+                    array2(v) = AscW(array(X))
+                    'MsgBox(array2(v))
+                    suma = arrayline(X)
+                    arrayspec(X) = array2(v) - suma
+                    array57(v) = ChrW(arrayspec(v)) 'Lo cambie por X
+                    'MsgBox(array57(v))
+                Next
+
+            Next
+
+            'End While
+            lector2.Close()
+            'MsgBox(contador)
+            arrayfinal(contador) = String.Join("", array57)
+            'MsgBox(array57(1))
+            'MsgBox(array57(2))
+
+
+            TextBox3.Text = arrayfinal(contador)
+
+
+
+
+
+        Else
+                MsgBox("4decript.txt NOT FOUND", MsgBoxStyle.Critical)
+
+        End If
+
+
+
+    End Sub
+
+    Private Sub Button4_Click_1(sender As Object, e As EventArgs) Handles Button4.Click
+        MsgBox("1- File 4decript.txt must stay in the desktop", MsgBoxStyle.Information)
+        MsgBox("2- You can encrypt and decrypt anytime, but .txt will be replaced so save it", MsgBoxStyle.Information)
+    End Sub
+End Class
